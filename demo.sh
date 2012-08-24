092eba543d0cf124778f4a2233627476d3c94d84
Arié Bénichou <arie.benichou@gmail.com>
2012-08-24 21:55:12 +0200

    Added restlet version property.

--- demo.sh
+++ demo.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-netstat -apn|grep :8080
+
+#echo ${MVN_HOME}
+#echo ${PATH}
+
+#netstat -apn|grep :8080
+
 kill -9 $( lsof -i:8080 -t )
+
 mvn clean
 mvn package
-mvn jetty:run-war & echo "\007" && sleep 3 && echo "\007" && sleep 2 && echo "\007" && sleep 1 && echo "\007" && google-chrome localhost:8080/blockplus/home
+
+mvn jetty:run-war & sleep 5 && echo "\007" && google-chrome localhost:8080/blockplus/home
