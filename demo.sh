532b7a3a23b8d3305f2b05ac55a7439e1b5ca672
Arié Bénichou <arie.benichou@gmail.com>
2012-08-24 21:54:50 +0200

    Kill any process using port: 8080. Little delay before lanching browser.

--- demo.sh
+++ demo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+netstat -apn|grep :8080
+kill -9 $( lsof -i:8080 -t )
+mvn clean
 mvn package
-mvn jetty:run-war
-echo "\007"
-google-chrome localhost:8080/blockplus/home
+mvn jetty:run-war & echo "\007" && sleep 3 && echo "\007" && sleep 2 && echo "\007" && sleep 1 && echo "\007" && google-chrome localhost:8080/blockplus/home
