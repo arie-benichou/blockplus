35c242427a361bce7bf0cdc584adf56c9361127f
arie.benichou <arie.benichou@gmail.com>
2013-02-13 04:55:28 +0100

    Removed dead code

--- src/main/java/serialization/PiecesSerializer.java
+++ src/main/java/serialization/PiecesSerializer.java
@@ -21,13 +21,10 @@ import java.lang.reflect.Type;
 
 import blockplus.piece.PieceType;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import com.google.gson.reflect.TypeToken;
 
 @Deprecated
 public final class PiecesSerializer implements JsonSerializer<PieceType> {
@@ -35,18 +32,6 @@ public final class PiecesSerializer implements JsonSerializer<PieceType> {
     @Override
     public JsonElement serialize(final PieceType piece, final Type typeOfSrc, final JsonSerializationContext context) {
         return new JsonPrimitive(piece.ordinal());
-    }
-
-    public static void main(final String[] args) {
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder = gsonBuilder.registerTypeAdapter(new TypeToken<PieceType>() {}.getType(), new PiecesSerializer());
-        final Gson gson = gsonBuilder.create();
-
-        final PieceType position = PieceType.get(7);
-        final String json = gson.toJson(position);
-        System.out.println(json);
-
     }
 
 }
