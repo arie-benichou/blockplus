28af4c3c8cd736b8b4d12f6bfdd9da4471ddfc08
Michel Daviot <michel@daviot.info>
2013-02-25 23:39:35 +0100

    score takes into account 15 bonus for all pieces

--- src/main/scala/blockplus/context/Scorer.scala
+++ src/main/scala/blockplus/context/Scorer.scala
@@ -2,12 +2,18 @@ package blockplus.context
 
 import blockplus.Color
 import scala.collection.JavaConversions._
+import blockplus.move.Move
 
 object Scorer {
   def apply(history: MoveHistory): Map[Color, Int] = (
     for (
       (color, moves) <- history.data.groupBy(_.getColor)
-    ) yield (color -> ((moves map (_.getPiece.getSelfPositions.size) sum) - 89)))
+    ) yield (color -> score(moves)))
     .toMap withDefaultValue -89
 
+  private def score(moves: Seq[Move]) =
+    (moves map value sum) - 89 +
+      (if (moves.size == 21) 15 else 0)
+
+  private def value(m: Move) = m.getPiece.getSelfPositions.size
 }
--- src/test/scala/blockplus/context/ScorerSpec.scala
+++ src/test/scala/blockplus/context/ScorerSpec.scala
@@ -11,15 +11,33 @@ import blockplus.move.Move
 import blockplus.move.Moves
 import blockplus.piece.PieceInterface
 import blockplus.piece.PieceType
+import scala.util.Random
 
 class ScorerSpec extends SpecificationWithJUnit with ScalaCheck {
   val initial = -(1 + 2 + 3 + 3 + 5 * 4 + 12 * 5)
   implicit val colorGen = Arbitrary(Gen.oneOf(Color.values))
   implicit val pieceGen = Arbitrary(Gen.oneOf(PieceType.values flatMap (_.iterator)))
+  implicit val allPiecesGen = Arbitrary(Gen.pick(21, PieceType.values filter (_.id != 0)) map (Random.shuffle(_)))
 
   "initial score should be -89" in {
     val scores = Scorer(MoveHistory())
     prop { (c: Color) => scores(c) must_== initial }
+  }
+
+  "after playing all pieces score should be 15" in {
+    prop {
+      (moves: Seq[PieceType]) =>
+        val allMovesPlayed = moves.foldLeft(MoveHistory()) {
+          case (hist, pieceType) => hist.play(Moves.getMove(Color.Blue, pieceType.iterator.next))
+        }
+        val scores = Scorer(allMovesPlayed)
+        scores(Color.Blue) must_== 15
+        prop { (c2: Color) =>
+          c2 != Color.Blue ==> {
+            scores(c2) must_== initial
+          }
+        }
+    }
   }
 
   "after playing one piece score should be -89+piece.size and -89 for other colors" in {
@@ -27,9 +45,10 @@ class ScorerSpec extends SpecificationWithJUnit with ScalaCheck {
       p.getId != 0 ==> {
         val scores = Scorer(MoveHistory().play(Moves.getMove(c, p)))
         scores(c) must_== initial + p.getSelfPositions.size
-        prop { (c2:Color) =>
-          c2!=c ==> {
-            scores(c2) must_== initial 
+
+        prop { (c2: Color) =>
+          c2 != c ==> {
+            scores(c2) must_== initial
           }
         }
       }
