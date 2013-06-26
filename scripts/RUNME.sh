30f89c783e0b614078796f2120bd35d7356b540d
arie.benichou <arie.benichou@gmail.com>
2013-06-26 11:57:02 +0200

    auto merging of javascript modules

--- scripts/RUNME.sh
+++ scripts/RUNME.sh
@@ -5,5 +5,7 @@ WHICH_MAVEN=$(which mvn);
 if $WHICH_MAVEN >/dev/null ; then
 	echo "Maven not found.";
 else
+	cat ./src/main/resources/www/data/modules/*.js > ./src/main/resources/www/data/modules.js
+	cat ./src/main/resources/www/data/modules/**/*.js >> ./src/main/resources/www/data/modules.js
 	mvn jetty:run;
 fi
