ef9140f2d1539b5bee80fa564526e07f080083c0
arie.benichou <arie.benichou@gmail.com>
2013-06-30 13:48:24 +0200

    Moved orginal javascript modules to private folder

--- scripts/update-js.sh
+++ scripts/update-js.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat ./src/main/resources/www/data/modules/*.js > ./src/main/resources/www/data/debug.js
-cat ./src/main/resources/www/data/modules/**/*.js >> ./src/main/resources/www/data/debug.js
-java -jar externals/closure-compiler.jar --js src/main/resources/www/data/debug.js > src/main/resources/www/data/modules.js 
+cat ./src/main/resources/www/private/modules/*.js > ./src/main/resources/www/blockplus.js
+cat ./src/main/resources/www/private/modules/**/*.js >> ./src/main/resources/www/blockplus.js
+java -jar externals/closure-compiler.jar --js src/main/resources/www/blockplus.js > src/main/resources/www/public/library/blockplus.min.js 
