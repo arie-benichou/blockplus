00c58fb3ee31dd657b8ea47c191c576d7ad80b5f
arie.benichou <arie.benichou@gmail.com>
2013-10-31 17:51:46 +0100

    extracted run method

--- src/main/scala/games/blokus/Main.scala
+++ src/main/scala/games/blokus/Main.scala
@@ -3,10 +3,11 @@ package games.blokus
 import components.Positions
 import components.Positions.Ordering
 import components.Positions.Position
-import games.blokus.Game.Move
+import games.blokus.Game._
 import games.blokus.Polyominos.Polyomino
 import games.blokus.Game.Color
 import games.blokus.Polyominos.Instances.Instance
+import abstractions.Context
 
 object Main {
 
@@ -32,7 +33,7 @@ object Main {
     normalizedInstance.translateBy(topLeftCorner - Positions.Origin)
   }
 
-  private def render(color: Color, light: Position, positions: Set[Position], instance: Instance): Unit = {
+  private def renderer(color: Color, light: Position, positions: Set[Position], instance: Instance): Unit = {
     println(""
       + "================\n"
       + color
@@ -45,25 +46,33 @@ object Main {
     )
   }
 
-  def main(args: Array[String]) {
+  def nullRenderer(color: Color, light: Position, positions: Set[Position], instance: Instance) {}
 
-    var context = Game.context
-    while (!context.isTerminal) {
+  def run(context: BlokusContext, renderer: (Color, Position, Set[Position], Instance) => Unit): BlokusContext = {
+    if (context.isTerminal) context
+    else {
       val options = Options.get(context.id, context.space, context.sideToPlay.values)
       val (light, polyomino, positions) = choose(options)
       val instance = positionsToInstance(polyomino, positions)
       val move = Move(context.id, instance)
-      context = context(move)
-      render(context.id, light, positions, instance)
-      context = context.forward
+      val nextContext = context(move)
+      renderer(nextContext.id, light, positions, instance)
+      run(nextContext.forward, renderer)
     }
+
+  }
+
+  def main(args: Array[String]) {
+
+    val initialContext = Game.context
+    val terminalContext = run(initialContext, renderer)
 
     println
     println("Game Over !")
     println
 
     //TODO sides iterator
-    context.sides.sides.foreach(side => {
+    terminalContext.sides.sides.foreach(side => {
       println(side._1)
       println(side._2.values.weight)
       side._2.values.polyominos.foreach(x => println("\n" + x.instances.head))
