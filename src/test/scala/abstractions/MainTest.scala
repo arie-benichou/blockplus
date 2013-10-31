235c249f3041f6907d5604cb67827eacd9410039
arie.benichou <arie.benichou@gmail.com>
2013-10-31 17:52:05 +0100

    Added integration test

---
+++ src/test/scala/abstractions/MainTest.scala
@@ -0,0 +1,35 @@
+package abstractions
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSpec
+import org.scalatest.junit.JUnitRunner
+
+import games.blokus.Game
+import games.blokus.Game.Color
+import games.blokus.Main
+
+@RunWith(classOf[JUnitRunner])
+class MainTest extends FunSpec {
+
+  describe("[Main]") {
+
+    it("should have this result") {
+      // TODO tester chemin + résultat => TODO add path of moves to Context
+      val expected = Map(
+        Color.Blue -> 23,
+        Color.Yellow -> 20,
+        Color.Red -> 7,
+        Color.Green -> 9
+      )
+      val terminalContext = Main.run(Game.context, Main.nullRenderer)
+
+      val actual = terminalContext.sides.sides.map(side => {
+        (side._1, side._2.values.weight)
+      })
+
+      assert(expected === actual)
+    }
+
+  }
+
+}
