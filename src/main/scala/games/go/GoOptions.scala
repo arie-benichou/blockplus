52c42e9873d3b0d43234bba8fb193c2b3f022081
Arié Bénichou <arie.benichou@gmail.com>
2014-03-23 20:58:57 +0100

    Removed hardcoded opponent

--- src/main/scala/games/go/GoOptions.scala
+++ src/main/scala/games/go/GoOptions.scala
@@ -5,18 +5,21 @@ import scala.collection.immutable.SortedSet
 
 object GoOptions {
 
+  private def opponent(character: Char) = {
+    if (character == 'O') 'X' else if (character == 'X') 'O' else error("Unknown Character")
+  }
+
   /**
    *  1) As long as there is at least one degree of freedom remaining once played
    *  2) Except if the opponent string will loose its last degree of freedom
    */
-  // TODO remove hardcoded opponent
   def apply(character: Char, board: GoBoard): Set[Position] = {
     val space = board.cells.filter(_._2 == '.')
-    val stringsForSpace = board.layer('.').strings
+    val stringsForSpace = board.layer('.').strings // TODO parameterize
     val islands = stringsForSpace.filter(_.out.size < 1).map(_.in.iterator.next)
-    val stringsForO = board.layer('O').strings
+    val stringsForO = board.layer(character).strings
     val suicides = stringsForO.filter(_.out.size == 1).map(_.out.iterator.next)
-    val stringsForX = board.layer('X').strings
+    val stringsForX = board.layer(opponent(character)).strings
     val captures = stringsForX.filter(_.out.size == 1).map(_.out.iterator.next)
     SortedSet() ++ space -- islands -- suicides ++ captures
   }
