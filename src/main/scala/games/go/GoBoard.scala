a083599c002b7f429be2d9b28834100c18c6c298
Arié Bénichou <arie.benichou@gmail.com>
2014-03-23 20:58:32 +0100

    Moved GoString object into GoBoard file

--- src/main/scala/games/go/GoBoard.scala
+++ src/main/scala/games/go/GoBoard.scala
@@ -90,6 +90,10 @@ object GoBoard {
 
 }
 
+sealed case class GoString(in: Set[Position], out: Set[Position]) {
+  override def toString = "Freedom: " + this.out.size + "\n" + this.in.mkString("\n") + "\n"
+}
+
 sealed case class Layer(character: Char, cells: Cells[Char]) {
 
   lazy val strings: List[GoString] = {
