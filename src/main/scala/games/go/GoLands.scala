823dc38eb5ac9be52c6495b4ef01366808b8bd0f
Arié Bénichou <arie.benichou@gmail.com>
2014-03-24 10:25:43 +0100

    Draft for protected lands

---
+++ src/main/scala/games/go/GoLands.scala
@@ -0,0 +1,50 @@
+package games.go
+
+import components.Positions._
+import scala.collection.immutable.SortedSet
+
+object GoLands {
+
+  private def opponent(character: Char) = {
+    if (character == 'O') 'X' else if (character == 'X') 'O' else error("Unknown Character")
+  }
+
+  def apply(character: Char, board: GoBoard): Set[Position] = {
+    val space = board.cells.filter(_._2 == '.')
+    val stringsForSpace = board.layer('.').strings // TODO parameterize
+    val islands = stringsForSpace.filter(_.out.size < 1).map(_.in.iterator.next)
+    val stringsForPlayer = board.layer(character).strings
+    val stringsForOpponent = board.layer(opponent(character)).strings
+    val effectiveIslands = islands.filter { p =>
+      stringsForPlayer.exists(_.out.contains(p)) &&
+        !stringsForOpponent.exists(_.out.contains(p))
+    }
+    SortedSet() ++ effectiveIslands
+  }
+
+  def main(args: Array[String]) {
+
+    val data = Array(
+      "OO.OO",
+      ".OOO.",
+      "OO.OO",
+      "O.O.O",
+      "OO.OO"
+    )
+
+    val board = GoBoard(data)
+    val lands = GoLands('O', board)
+
+    println("=================================")
+    println
+    println(board)
+    println("Lands for 'O' :\n")
+    lands.foreach { land =>
+      println(" " + land)
+    }
+    println
+    println("=================================")
+
+  }
+
+}
